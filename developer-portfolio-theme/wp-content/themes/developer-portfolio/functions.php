9e09e82c45c23123a8ab8f226132e77761ba479c
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-04-29 17:37:43 +0530

    Milestone 1 (#15)
    * starting this milestone now
    
    * add footer hook in footer template
    
    * add header hook in header template
    
    * add logic to display posts using the loop
    
    * fix header template by moving header hook from title tag
    
    * add image for theme preview
    
    * add stylesheet using enqueue function for theme

--- developer-portfolio-theme/wp-content/themes/developer-portfolio/functions.php
+++ developer-portfolio-theme/wp-content/themes/developer-portfolio/functions.php
@@ -4,3 +4,9 @@
  *
  * @package Developer_Portfolio
  */
+
+function theme_style() 
+{
+    wp_enqueue_style("theme-style", get_template_directory_uri() . "/style.css");
+}
+add_action("wp_enqueue_scripts", "theme_style");
--- developer-portfolio-theme/wp-content/themes/developer-portfolio/index.php
+++ developer-portfolio-theme/wp-content/themes/developer-portfolio/index.php
@@ -4,3 +4,22 @@
  *
  * @package Developer_Portfolio
  */
+get_header();
+
+if (have_posts()) :
+    while (have_posts()) :
+        the_post();
+        ?>
+        <h2><?php the_title();?></h2>
+        <?php the_time();
+        the_content();
+        
+        ?>
+        <?php
+    endwhile;
+    else :
+        _e("No posts found", "developer-portfolio");
+    endif;
+
+get_footer();
+?>

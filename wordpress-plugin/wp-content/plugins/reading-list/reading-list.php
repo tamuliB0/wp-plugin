64f6b3bddf34617c87b0c73f0d3cf663c60723d9
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-05-12 17:33:27 +0530

    Milestone 1 (#20)
    * starting this milestone now
    
    * remove Wordpress core and environment files from tracking
    
    * remove Wordpress core and environment files from tracking
    
    * add composer.lock file
    
    * Add activation hook callback body for database table creation
    
    * remove comment from function body to fix PHPCS issues

--- wordpress-plugin/wp-content/plugins/reading-list/reading-list.php
+++ wordpress-plugin/wp-content/plugins/reading-list/reading-list.php
@@ -5,6 +5,8 @@
  * Version:     1.0.0
  * Author:      tamuliB0
  * Text Domain: reading-list
+ *
+ * @package Reading List
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -12,11 +14,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 register_activation_hook( __FILE__, 'reading_list_activate' );
+
+/**
+ * Runs on plugin activation.
+ */
 function reading_list_activate() {
-	// TODO: create the database table in M1
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . 'reading_list';
+
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+	id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	title varchar(255) NOT NULL,
+	author varchar(255) NOT NULL,
+	status varchar(20) NOT NULL DEFAULT 'to-read',
+	notes text,
+	created_at datetime NOT NULL,
+	PRIMARY KEY  (id)
+	) $charset_collate;";
+
+	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+	dbDelta( $sql );
 }
 
 register_deactivation_hook( __FILE__, 'reading_list_deactivate' );
+
+/**
+ * Runs on plugin deactivation.
+ */
 function reading_list_deactivate() {
-	// TODO: clean up in M5
 }
